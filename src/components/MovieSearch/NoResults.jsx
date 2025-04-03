7fadd37dbaca150e0874f13f6601e283b3a3d839
RoystonDAlmeida <roystonjan1@hotmail.com>
2025-04-04 02:02:16 +0530

    No results component

---
+++ src/components/MovieSearch/NoResults.jsx
@@ -0,0 +1,9 @@
+function NoResults() {
+    return (
+      <div className="text-center text-slate-400">
+        No movies match the current filters
+      </div>
+    );
+  }
+  
+export default NoResults;
