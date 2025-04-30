419f9433e9bb13eccac524797f7a9d41116bff11
RoystonDAlmeida <roystonjan1@hotmail.com>
2025-04-30 22:27:01 +0530

    Added whitespace-nowrap attribute for results found in mobile view devices

--- src/components/MovieSearch/ResultsCount.jsx
+++ src/components/MovieSearch/ResultsCount.jsx
@@ -1,9 +1,9 @@
 function ResultsCount({ numResults }) {
     return (
-      <div className="text-slate-300">
+      <div className="text-slate-300 whitespace-nowrap">
         {numResults} {numResults === 1 ? 'result' : 'results'} found
       </div>
     );
-  }
+}
   
 export default ResultsCount;  
